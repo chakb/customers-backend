84849bf721dcf8574828ce0fbf3fd1074e92555f
chakb <emiliosdh@gmail.com>
2022-04-27 02:49:35 +0200

    tests added: customer api get by id

--- tests/customer_api.test.js
+++ tests/customer_api.test.js
@@ -38,3 +38,35 @@ describe('customer creation', () => {
     expect(response.body.errors).toBeDefined();
   });
 });
+
+describe('single customer retrieval', () => {
+  let customer;
+  beforeEach(async () => {
+    customer = await api.post('/customer').send({
+      name: 'John',
+      surname: 'Doe',
+      email: 'johndoe@doe.com',
+      birthDate: '2000-01-01',
+    });
+  });
+
+  test('should suceed with valid id', async () => {
+    const response = await api
+      .get(`/customer/${customer.body.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+    expect(response.body.id).toBe(customer.body.id);
+    expect(response.body.name).toBe(customer.body.name);
+    expect(response.body.surname).toBe(customer.body.surname);
+    expect(response.body.email).toBe(customer.body.email);
+    expect(response.body.birthDate).toBe(customer.body.birthDate);
+  });
+
+  test('should fail with invalid id', async () => {
+    const response = await api
+      .get('/customer/1234-1234-1234')
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+    expect(response.body.error).toBeDefined();
+  });
+});
