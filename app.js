f3aec14c8142ef7221203cd6744b5cff443e57f7
chakb <emiliosdh@gmail.com>
2022-04-27 04:46:24 +0200

    added api documentation with swagger

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
 const customersRouter = require('./controllers/customers');
-
 app.use(express.json());
-
 app.use('/customer', customersRouter);
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
